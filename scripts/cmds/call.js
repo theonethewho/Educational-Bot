b04f8b3b63450318623c7c48b09adc82c6c24015
malikjamss <malikjamaica9@gmail.com>
2024-09-28 08:16:29 +0800

    Create call.js

---
+++ scripts/cmds/call.js
@@ -0,0 +1,16 @@
+module.exports = {
+    config: {
+        name: "call",
+        role: 0,
+        author: "Micazhla",
+        longDescription: "Not a command"
+    },
+    onStart: async function ({ message }) {
+        message.reply("How can I help you?")
+    },
+    onChat: async function ({ message, event }) {
+        if (event.body && event.body.toLowerCase() === "rosa") {
+            message.reply("Hello, how may I help?")
+        }
+    }
+};
